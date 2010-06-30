1465803797dea44b2eadea4a793e82b251b60648
Edgar <edgargonzalez@gmail.com>
2010-06-30 17:44:34 -0430

    added more specs

--- spec/rrdsimple_spec.rb
+++ spec/rrdsimple_spec.rb
@@ -34,7 +34,7 @@ describe "RRDSimple" do
     # The bucket should have the counter
     redis.get('k:0').to_i.should == 4
     # The epoch should be consistent
-    redis.get('k:epoch').to_i.should == jan01.to_i / 60
+    rr.last_epoch('k').should == jan01.to_i / 60
     v = rr.values('k')
     v.size.should == 1
     v[0][:value].should == 4
@@ -50,7 +50,9 @@ describe "RRDSimple" do
     redis.get('k:0').to_i.should == 4
     redis.get('k:1').to_i.should == 2
     # The epoch should be consistent
-    redis.get('k:epoch').to_i.should == (jan01 + 60).to_i / 60
+    rr.last_epoch('k').should == (jan01 + 60).to_i / 60
+
+    # values
     v = rr.values('k')
     v.size.should == 2
 
@@ -65,25 +67,118 @@ describe "RRDSimple" do
     Time.at(v[1][:epoch]*60).utc.should == jan01
 
 
-    jump 120 # jump two minute
+    jump 60*2 # jump two minute
     rr.incr("k").should == 1
     rr.incr("k").should == 2
+    rr.incr("k").should == 3
 
     # The bucket should have the counter
     redis.get('k:0').to_i.should == 4
     redis.get('k:1').to_i.should == 2
     redis.exists('k:2').should be_false
-    redis.get('k:3').to_i.should == 2
+    redis.get('k:3').to_i.should == 3
     # The epoch should be consistent
-    redis.get('k:epoch').to_i.should == (jan01 + 60 + 120).to_i / 60
+    rr.last_epoch('k').should == (jan01 + 60 + 120).to_i / 60
 
+    # values
+    v = rr.values('k')
+    v.size.should == 3
 
-jump 60*30
+    v[0][:value].should == 3
+    # The following to assertion are equivalent
+    v[0][:epoch].should == (jan01 + 60 + 60*2).to_i / 60
+    Time.at(v[0][:epoch]*60).should == jan01 + 60 + 60*2
+
+    v[1][:value].should == 2
+    # The following to assertion are equivalent
+    v[1][:epoch].should == (jan01 + 60).to_i / 60
+    Time.at(v[1][:epoch]*60).should == jan01 + 60
+
+    v[2][:value].should == 4
+    # The following to assertion are equivalent
+    v[2][:epoch].should == jan01.to_i / 60
+    Time.at(v[2][:epoch]*60).utc.should == jan01
+
+    jump 60*30 # jump 30 minutes
+
+    rr.incr('k').should == 1
+    # The bucket should have the counter
+    redis.get('k:0').to_i.should == 4
+    redis.get('k:1').to_i.should == 2
+    redis.exists('k:2').should be_false
+    redis.get('k:3').to_i.should == 3
+    (4..32).each do |i|
+      redis.exists("k:#{i}").should be_false
+    end
+    redis.get('k:33').to_i.should == 1
+
+    # The epoch should be consistent
+    rr.last_epoch('k').should == (jan01 + 60 + 60*2 + 60*30).to_i / 60
+
+    # values
+    v = rr.values('k')
+    v.size.should == 4
+
+    v[0][:value].should == 1
+    # The following to assertion are equivalent
+    v[0][:epoch].should == (jan01 + 60 + 60*2 + 60*30).to_i / 60
+    Time.at(v[0][:epoch]*60).should == jan01 + 60 + 60*2 + 60*30
+
+    v[1][:value].should == 3
+    # The following to assertion are equivalent
+    v[1][:epoch].should == (jan01 + 60 + 60*2).to_i / 60
+    Time.at(v[1][:epoch]*60).should == jan01 + 60 + 60*2
+
+    v[2][:value].should == 2
+    # The following to assertion are equivalent
+    v[2][:epoch].should == (jan01 + 60).to_i / 60
+    Time.at(v[2][:epoch]*60).should == jan01 + 60
+
+    v[3][:value].should == 4
+    # The following to assertion are equivalent
+    v[3][:epoch].should == jan01.to_i / 60
+    Time.at(v[3][:epoch]*60).utc.should == jan01
+
+    jump 60*45 # jump 45 minutes
+    rr.last_epoch('k').should == (jan01 + 60 + 60*2 + 60*30).to_i / 60
+
+    rr.incr('k').should == 1
+    rr.incr('k').should == 2
+    rr.incr('k').should == 3
+    rr.incr('k').should == 4
+    rr.incr('k').should == 5
+
+    # The bucket should have the counter
+    (0..17).each do |i|
+      redis.exists("k:#{i}").should be_false
+    end
+    redis.get('k:18').to_i.should == 5
+    (19..32).each do |i|
+      redis.exists("k:#{i}").should be_false
+    end
+    redis.get('k:33').to_i.should == 1
+    (34..59).each do |i|
+      redis.exists("k:#{i}").should be_false
+    end
+
+    # values
+    v = rr.values('k')
+    v.size.should == 2
+
+    v[0][:value].should == 5
+    # The following to assertion are equivalent
+    v[0][:epoch].should == (jan01 + 60 + 60*2 + 60*30 + 60*45).to_i / 60
+    Time.at(v[0][:epoch]*60).should == jan01 + 60 + 60*2 + 60*30 + 60*45
+
+    v[1][:value].should == 1
+    # The following to assertion are equivalent
+    v[1][:epoch].should == (jan01 + 60 + 60*2 + 60*30).to_i / 60
+    Time.at(v[1][:epoch]*60).should == jan01 + 60 + 60*2 + 60*30
 
 
     rr.clear('k')
     redis.exists("k:epoch").should be_false
-    60.times do |i|
+    (0..59).each do |i|
       redis.exists("k:{i}").should be_false
     end
   end
